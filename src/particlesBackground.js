c759b493cb5fe14d6289d981b7d9f94c2dd94c51
minari515 <126942302+minari515@users.noreply.github.com>
2023-03-13 15:16:20 +0900

    スタート画面のUIに関係する処理

---
+++ src/particlesBackground.js
@@ -0,0 +1,12 @@
+import Particles from 'react-tsparticles'
+import ParticlseConfig from "./particles"
+
+const particlesBackground = () => {
+	return (
+		<Particles params={ParticlseConfig}>
+
+		</Particles>
+	)
+}
+
+export default particlesBackground
